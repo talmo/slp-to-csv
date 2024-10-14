d38d1c2de0a496c2a803e351d9490bebb4f781a1
Talmo Pereira <talmo@salk.edu>
2024-10-14 14:30:01 -0400

    Add starter function

--- slp_to_csv/main.py
+++ slp_to_csv/main.py
@@ -1,1 +1,13 @@
 """Main module for SLP to CSV conversion."""
+
+import sleap_io as sio
+
+
+def load_slp(slp_path: str) -> sio.Labels:
+    """Load SLP file into SLEAP Labels object.
+    Args:
+        slp_path: Path to SLP file.
+    Returns:
+        SLEAP Labels object.
+    """
+    return sio.load_file(slp_path)
